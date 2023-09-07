1cf223d5e38c8081aa413006fea4898d59c2e220
acmyu <amycmyu@gmail.com>
2023-09-07 02:44:10 -0400

    Scripts to pull fathomnet data

---
+++ scripts/pull_wikipedia_data.py
@@ -0,0 +1,5 @@
+# Save data from wikipedia for each concept and its genus. 
+# If the page doesn't exist or is very short, use its ancestor's wiki page (excluding 1st paragraph), replacing the ancestor name with the concept name
+# Sanitize the concept names (eg. "Acanthascinae sp. 1-4 complex", "Cirrata \"egg\"", "Holothuroidea/Actiniaria", "Polychelidae 2", "Spongosorites (right)", "Uroptychus n.")
+# What to do with cases like "1-gallon paint bucket" if there's no wiki page? Ignore them? Use the last word (eg. "bucket", "laser")?
+
