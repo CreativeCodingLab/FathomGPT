ed31c279fc993a7f22b6746259b1624d3ca1b5ae
acmyu <amycmyu@gmail.com>
2023-09-21 22:41:42 -0400

    add wikipedia summary to concepts embedding

--- scripts/gen_embeddings.py
+++ scripts/gen_embeddings.py
@@ -4,17 +4,23 @@
 import pandas as pd
 import tiktoken
 
+import os
+os.environ["OPENAI_API_KEY"] = #add key
+
 from openai.embeddings_utils import get_embedding
 # embedding model parameters
 embedding_model = "text-embedding-ada-002"
 embedding_encoding = "cl100k_base"  # this the encoding for text-embedding-ada-002
 # load & inspect dataset
-input_datapath = "data/concepts.csv"  # to save space, we provide a pre-filtered dataset
+input_datapath = "data/concepts_desc.csv" 
 df = pd.read_csv(input_datapath)
+df = df[["concepts", "description", "related"]]
 df = df.dropna()
-print(df.head(2))
+df["combined"] = (
+    df.description.str.strip()+" ("+df.related.str.strip()+")"
+)
 
 encoding = tiktoken.get_encoding(embedding_encoding)
 
-df["embedding"] = df.concepts.apply(lambda x: get_embedding(x, engine=embedding_model))
-df.to_csv("data/concepts_embeddings.csv")
+df["embedding"] = df.combined.apply(lambda x: get_embedding(x, engine=embedding_model, ))
+df.to_csv("data/concepts_desc_embeddings.csv")
--- scripts/pull_wikipedia_data.py
+++ scripts/pull_wikipedia_data.py
@@ -3,3 +3,46 @@
 # Sanitize the concept names (eg. "Acanthascinae sp. 1-4 complex", "Cirrata \"egg\"", "Holothuroidea/Actiniaria", "Polychelidae 2", "Spongosorites (right)", "Uroptychus n.", "Aspidodiadema cf. hawaiiense")
 # What to do with cases like "1-gallon paint bucket" if there's no wiki page? Ignore them? Use the last word (eg. "bucket", "laser")?
 
+import pandas as pd
+import wikipedia
+
+
+input_datapath = "data/concepts.csv"
+df = pd.read_csv(input_datapath)
+df = df.dropna()
+print(df.head(2))
+
+description = []
+related_terms = []
+for concept in df.concepts:
+    print(concept)
+    name = concept
+    try:
+        related = wikipedia.search(concept)
+        if len(related) > 0:
+            name = related[0]
+        if name != concept:
+            related_terms.append(", ".join(related))
+        elif len(related)>1:
+            related_terms.append(", ".join(related[1:]))
+        else:
+            related_terms.append("")
+    except:
+        related_terms.append("")
+        
+    try:
+        summary = wikipedia.summary(name, sentences=2)
+        summary = summary.split('\n')
+        description.append(summary[0])
+    except:
+        description.append("")
+
+
+df['description'] = description
+df['related'] = related_terms
+df.to_csv("data/concepts_desc.csv")
+
+
+#print(wikipedia.search("Octogonade mediterranea"))
+#print(wikipedia.summary("Bathypolypus", sentences=2).split("\n"))
+
