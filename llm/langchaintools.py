17eb73dfe4d6eedf33f2ea29292bfaa3226e8a70
itznabin <itznabin@gmail.com>
2023-11-09 10:31:33 -0500

    main/fix bug

--- llm/langchaintools.py
+++ llm/langchaintools.py
@@ -441,17 +441,17 @@ def get_Response(prompt, messages=[], isEventStream=False):
                     break
                 else:
                     modifiedMessages.append({"role":"function","content":result,"name": function_name})
-                    modifiedMessages.append({"role":"system","content":"Is the result generated in previous query enough to response the prompt. Prompt: {prompt} Output either 'True' or 'False', nothing else"})
-                    response2 = openai.ChatCompletion.create(
-                        model="gpt-3.5-turbo-0613",
-                        messages=modifiedMessages,
-                        temperature=0,
-                    )
-                    if("TRUE" in response2["choices"][0]["message"]["content"].upper()):
-                        modifiedMessages = modifiedMessages[:-2] 
-                        break
-                    else:
-                        modifiedMessages = modifiedMessages[:-1] 
+                    #modifiedMessages.append({"role":"system","content":"Is the result generated in previous query enough to response the prompt. Prompt: {prompt} Output either 'True' or 'False', nothing else"})
+                    #response2 = openai.ChatCompletion.create(
+                    #    model="gpt-3.5-turbo-0613",
+                    #    messages=modifiedMessages,
+                    #    temperature=0,
+                    #)
+                    #if("TRUE" in response2["choices"][0]["message"]["content"].upper()):
+                    #    modifiedMessages = modifiedMessages[:-2] 
+                    #    break
+                    #else:
+                    #    modifiedMessages = modifiedMessages[:-1] 
             else:
                 raise ValueError("No function named '{function_name}' in the global scope")
         else:
@@ -555,7 +555,7 @@ def get_Response(prompt, messages=[], isEventStream=False):
 #for v in get_Response("Find me images of moon jellyfish in Monterey bay and depth less than 5k meters", isEventStream=True):
 #for v in get_Response("Find me images containing tentacles in Monterey bay and depth less than 5k meters", isEventStream=True):
 #for v in get_Response("Find me images of ray-finned creatures in Monterey bay and depth less than 5k meters", isEventStream=True):
-for v in get_Response("Find me images of deep sea fish in Monterey bay and depth less than 5k meters", isEventStream=True):
+for v in get_Response("Find me images of Aurelia Aurita", isEventStream=True):
     print(v)
 
 #test_msgs = [{'role': 'user', 'content': 'find me images of aurelia aurita'}, {'role': 'assistant', 'content': "{'outputType': 'image', 'responseText': 'Images of Aurelia Aurita', 'vegaSchema': {}, 'species': [{'url': 'https://fathomnet.org/static/m3/framegrabs/Ventana/images/3405/00_05_46_16.png', 'image_id': 2593314, 'concept': 'Aurelia aurita', 'id': 2593317}, {'url': 'https://fathomnet.org/static/m3/framegrabs/Ventana/images/3184/02_40_29_11.png', 'image_id': 2593518, 'concept': 'Aurelia aurita', 'id': 2593520}, {'url': 'https://fathomnet.org/static/m3/framegrabs/Doc%20Ricketts/images/0970/06_02_03_18.png', 'image_id': 2598130, 'concept': 'Aurelia aurita', 'id': 2598132}, {'url': 'https://fathomnet.org/static/m3/framegrabs/Ventana/images/3082/05_01_45_07.png', 'image_id': 2598562, 'concept': 'Aurelia aurita', 'id': 2598564}, {'url': 'https://fathomnet.org/static/m3/framegrabs/Doc%20Ricketts/images/0971/03_42_04_04.png', 'image_id': 2600144, 'concept': 'Aurelia aurita', 'id': 2600146}, {'url': 'https://fathomnet.org/static/m3/framegrabs/Ventana/images/3219/00_02_48_21.png', 'image_id': 2601105, 'concept': 'Aurelia aurita', 'id': 2601107}, {'url': 'https://fathomnet.org/static/m3/framegrabs/Ventana/images/3185/00_05_28_02.png', 'image_id': 2601178, 'concept': 'Aurelia aurita', 'id': 2601180}, {'url': 'https://fathomnet.org/static/m3/framegrabs/Ventana/images/3082/04_59_01_12.png', 'image_id': 2601466, 'concept': 'Aurelia aurita', 'id': 2601468}, {'url': 'https://fathomnet.org/static/m3/framegrabs/Ventana/images/3184/02_40_58_22.png', 'image_id': 2603507, 'concept': 'Aurelia aurita', 'id': 2603509}, {'url': 'https://fathomnet.org/static/m3/framegrabs/Ventana/stills/2000/236/02_33_01_18.png', 'image_id': 2604817, 'concept': 'Aurelia aurita', 'id': 2604819}]}"}]
