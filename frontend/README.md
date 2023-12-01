543216ab88e4d41f9f24b3b00fab5e4dfe1102ec
navinate <treycluff@gmail.com>
2023-12-01 13:45:15 -0500

    added frontend app

---
+++ frontend/README.md
@@ -0,0 +1,3 @@
+# ovai frontend
+
+Frontend interface for the Purdue research group creating AI agents to query the Fathom.net database
---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import '../app.css';
+</script>
+
+<header>
+	<h1>OVAI GPT Prototype</h1>
+</header>
+<slot />
+
+<style>
+	header {
+		width: 100%;
+		top: 0;
+		display: grid;
+		grid-template-columns: 20% 80%;
+		min-height: 4rem;
+		background-color: var(--accent-light);
+	}
+
+	h1 {
+		grid-area: 1 / 1 / 2 / 2;
+		place-self: center;
+	}
+</style>
