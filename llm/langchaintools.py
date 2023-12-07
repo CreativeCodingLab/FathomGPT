dc526ba4b573e762aeafb74b41c245a1efcba786
acmyu <amycmyu@gmail.com>
2023-12-07 12:25:13 -0500

    cleanup

--- llm/langchaintools.py
+++ llm/langchaintools.py
@@ -15,7 +15,7 @@ from openai.embeddings_utils import get_embedding, cosine_similarity
 import pymssql
 import re
 import time
-#from server.models import Interaction
+from server.models import Interaction
 
 from langchain.tools.base import StructuredTool
 from langchain.chat_models import ChatOpenAI
@@ -623,8 +623,8 @@ def get_Response(prompt, messages=[], isEventStream=False, db_obj=None):
         output["table"] = result
         
     if isEventStream:
-        #Interaction.objects.create(main_object=db_obj, request=prompt, response=output)
-        #output['guid']=str(db_obj.id)
+        Interaction.objects.create(main_object=db_obj, request=prompt, response=output)
+        output['guid']=str(db_obj.id)
         event_data = {
             "result": output
         }
@@ -657,7 +657,7 @@ def get_Response(prompt, messages=[], isEventStream=False, db_obj=None):
 #print(json.dumps(get_Response("Find me images of moon jelliefish in Monterey bay and depth less than 5k meters")))
 #print(json.dumps(get_Response("Find me images of rattails in Monterey bay and depth less than 5k meters")))
 
-for v in get_Response("Find me images of starfish in Monterey bay and depth less than 5k meters", isEventStream=True):
+#for v in get_Response("Find me images of starfish in Monterey bay and depth less than 5k meters", isEventStream=True):
 #for v in get_Response("Find me images of moon jellyfish in Monterey bay and depth less than 5k meters", isEventStream=True):
 #for v in get_Response("Find me images of creatures with tentacles in Monterey bay and depth less than 5k meters", isEventStream=True):
 #for v in get_Response("Find me images of ray-finned creatures in Monterey bay and depth less than 5k meters", isEventStream=True):
@@ -668,7 +668,7 @@ for v in get_Response("Find me images of starfish in Monterey bay and depth less
 #for v in get_Response("What species belong to the genus Aurelia", isEventStream=True):
 #for v in get_Response("Show me the taxonomy of moon jelly", isEventStream=True):
 #for v in get_Response("Display a bar chart showing the temperature ranges for Aurelia Aurita and Pycnopodia helianthoides from 0°C to 20 in 5°C increments", isEventStream=True):
-    print(v)
+    #print(v)
 
 #test_msgs = [{'role': 'user', 'content': 'find me images of aurelia aurita'}, {'role': 'assistant', 'content': "{'outputType': 'image', 'responseText': 'Images of Aurelia Aurita', 'vegaSchema': {}, 'species': [{'url': 'https://fathomnet.org/static/m3/framegrabs/Ventana/images/3405/00_05_46_16.png', 'image_id': 2593314, 'concept': 'Aurelia aurita', 'id': 2593317}, {'url': 'https://fathomnet.org/static/m3/framegrabs/Ventana/images/3184/02_40_29_11.png', 'image_id': 2593518, 'concept': 'Aurelia aurita', 'id': 2593520}, {'url': 'https://fathomnet.org/static/m3/framegrabs/Doc%20Ricketts/images/0970/06_02_03_18.png', 'image_id': 2598130, 'concept': 'Aurelia aurita', 'id': 2598132}, {'url': 'https://fathomnet.org/static/m3/framegrabs/Ventana/images/3082/05_01_45_07.png', 'image_id': 2598562, 'concept': 'Aurelia aurita', 'id': 2598564}, {'url': 'https://fathomnet.org/static/m3/framegrabs/Doc%20Ricketts/images/0971/03_42_04_04.png', 'image_id': 2600144, 'concept': 'Aurelia aurita', 'id': 2600146}, {'url': 'https://fathomnet.org/static/m3/framegrabs/Ventana/images/3219/00_02_48_21.png', 'image_id': 2601105, 'concept': 'Aurelia aurita', 'id': 2601107}, {'url': 'https://fathomnet.org/static/m3/framegrabs/Ventana/images/3185/00_05_28_02.png', 'image_id': 2601178, 'concept': 'Aurelia aurita', 'id': 2601180}, {'url': 'https://fathomnet.org/static/m3/framegrabs/Ventana/images/3082/04_59_01_12.png', 'image_id': 2601466, 'concept': 'Aurelia aurita', 'id': 2601468}, {'url': 'https://fathomnet.org/static/m3/framegrabs/Ventana/images/3184/02_40_58_22.png', 'image_id': 2603507, 'concept': 'Aurelia aurita', 'id': 2603509}, {'url': 'https://fathomnet.org/static/m3/framegrabs/Ventana/stills/2000/236/02_33_01_18.png', 'image_id': 2604817, 'concept': 'Aurelia aurita', 'id': 2604819}]}"}]
 
