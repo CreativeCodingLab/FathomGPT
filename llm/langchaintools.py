fea22f9308b6afed6931643a3c3cce196e1b00a0
acmyu <amycmyu@gmail.com>
2023-12-07 11:52:23 -0500

    output intermediate results and sci names

--- llm/langchaintools.py
+++ llm/langchaintools.py
@@ -15,7 +15,7 @@ from openai.embeddings_utils import get_embedding, cosine_similarity
 import pymssql
 import re
 import time
-from server.models import Interaction
+#from server.models import Interaction
 
 from langchain.tools.base import StructuredTool
 from langchain.chat_models import ChatOpenAI
@@ -448,6 +448,7 @@ def get_Response(prompt, messages=[], isEventStream=False, db_obj=None):
     isSpeciesData = False
     result = None
     curLoopCount = 0
+    allResults = {}
 
     if isEventStream:
         event_data = {
@@ -485,13 +486,18 @@ def get_Response(prompt, messages=[], isEventStream=False, db_obj=None):
                             pass
             function_to_call = globals().get(function_name)
             if function_to_call:
+                result = function_to_call(**args)
                 if isEventStream:
                     event_data = {
-                        "message": availableFunctionsDescription[function_name]
+                        "message": availableFunctionsDescription[function_name],
                     }
+                    if isinstance(result, str):
+                        event_data["message"] = event_data["message"]+":\n"+result
+                        allResults[function_name] = result
+                    print(event_data)
                     sse_data = f"data: {json.dumps(event_data)}\n\n"
                     yield sse_data
-                result = function_to_call(**args)
+                
                 if(function_name=="generateSQLQuery"):
 
                     
@@ -589,6 +595,9 @@ def get_Response(prompt, messages=[], isEventStream=False, db_obj=None):
         }
     if "heatmap" in prompt:
         summaryPromptResponse["outputType"] = "heatmap"
+    
+    if "getScientificNamesFromDescription" in allResults:
+        output["responseText"] = output["responseText"] + "\nScentific names: " + allResults["getScientificNamesFromDescription"]
 
     if summaryPromptResponse["outputType"] == 'image' and result == None:
         output["outputType"] = 'text' 
@@ -614,8 +623,8 @@ def get_Response(prompt, messages=[], isEventStream=False, db_obj=None):
         output["table"] = result
         
     if isEventStream:
-        Interaction.objects.create(main_object=db_obj, request=prompt, response=output)
-        output['guid']=str(db_obj.id)
+        #Interaction.objects.create(main_object=db_obj, request=prompt, response=output)
+        #output['guid']=str(db_obj.id)
         event_data = {
             "result": output
         }
@@ -648,7 +657,7 @@ def get_Response(prompt, messages=[], isEventStream=False, db_obj=None):
 #print(json.dumps(get_Response("Find me images of moon jelliefish in Monterey bay and depth less than 5k meters")))
 #print(json.dumps(get_Response("Find me images of rattails in Monterey bay and depth less than 5k meters")))
 
-#for v in get_Response("Find me images of starfish in Monterey bay and depth less than 5k meters", isEventStream=True):
+for v in get_Response("Find me images of starfish in Monterey bay and depth less than 5k meters", isEventStream=True):
 #for v in get_Response("Find me images of moon jellyfish in Monterey bay and depth less than 5k meters", isEventStream=True):
 #for v in get_Response("Find me images of creatures with tentacles in Monterey bay and depth less than 5k meters", isEventStream=True):
 #for v in get_Response("Find me images of ray-finned creatures in Monterey bay and depth less than 5k meters", isEventStream=True):
@@ -659,7 +668,7 @@ def get_Response(prompt, messages=[], isEventStream=False, db_obj=None):
 #for v in get_Response("What species belong to the genus Aurelia", isEventStream=True):
 #for v in get_Response("Show me the taxonomy of moon jelly", isEventStream=True):
 #for v in get_Response("Display a bar chart showing the temperature ranges for Aurelia Aurita and Pycnopodia helianthoides from 0°C to 20 in 5°C increments", isEventStream=True):
-#    print(v)
+    print(v)
 
 #test_msgs = [{'role': 'user', 'content': 'find me images of aurelia aurita'}, {'role': 'assistant', 'content': "{'outputType': 'image', 'responseText': 'Images of Aurelia Aurita', 'vegaSchema': {}, 'species': [{'url': 'https://fathomnet.org/static/m3/framegrabs/Ventana/images/3405/00_05_46_16.png', 'image_id': 2593314, 'concept': 'Aurelia aurita', 'id': 2593317}, {'url': 'https://fathomnet.org/static/m3/framegrabs/Ventana/images/3184/02_40_29_11.png', 'image_id': 2593518, 'concept': 'Aurelia aurita', 'id': 2593520}, {'url': 'https://fathomnet.org/static/m3/framegrabs/Doc%20Ricketts/images/0970/06_02_03_18.png', 'image_id': 2598130, 'concept': 'Aurelia aurita', 'id': 2598132}, {'url': 'https://fathomnet.org/static/m3/framegrabs/Ventana/images/3082/05_01_45_07.png', 'image_id': 2598562, 'concept': 'Aurelia aurita', 'id': 2598564}, {'url': 'https://fathomnet.org/static/m3/framegrabs/Doc%20Ricketts/images/0971/03_42_04_04.png', 'image_id': 2600144, 'concept': 'Aurelia aurita', 'id': 2600146}, {'url': 'https://fathomnet.org/static/m3/framegrabs/Ventana/images/3219/00_02_48_21.png', 'image_id': 2601105, 'concept': 'Aurelia aurita', 'id': 2601107}, {'url': 'https://fathomnet.org/static/m3/framegrabs/Ventana/images/3185/00_05_28_02.png', 'image_id': 2601178, 'concept': 'Aurelia aurita', 'id': 2601180}, {'url': 'https://fathomnet.org/static/m3/framegrabs/Ventana/images/3082/04_59_01_12.png', 'image_id': 2601466, 'concept': 'Aurelia aurita', 'id': 2601468}, {'url': 'https://fathomnet.org/static/m3/framegrabs/Ventana/images/3184/02_40_58_22.png', 'image_id': 2603507, 'concept': 'Aurelia aurita', 'id': 2603509}, {'url': 'https://fathomnet.org/static/m3/framegrabs/Ventana/stills/2000/236/02_33_01_18.png', 'image_id': 2604817, 'concept': 'Aurelia aurita', 'id': 2604819}]}"}]
 
